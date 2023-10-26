b00e1ffeabcbc8d26e5c3b31fa42e956fa15b0d4
Tasneem <thalim.glam@gmail.com>
2023-10-26 16:56:41 -0700

    Finally the 5 days display is showing correct values

--- assets/script.js
+++ assets/script.js
@@ -28,16 +28,38 @@ async function searchWeather(cityName) {
         }).then(function (data5) {
           console.log(data5);
 
-          for (i = 1; i < 40; i = i + 8) {
-            //document.querySelector(".city").innerHTML = data.name;
-           // document.querySelector(".temp").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
-           // document.querySelector(".humidity").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
-           // document.querySelector(".wind").innerHTML = Math.ceil(data5.list[i].wind.speed) + " km/h";
-           console.log(data5.city.name)           //city name
-           console.log(data5.list[i].dt_txt)      //date
-           console.log(data5.list[i].main.temp)  //temp
-           console.log(data5.list[i].main.humidity) //humidity
-           console.log(data5.list[i].wind.speed) //wind-speed
+          for (i = 0; i < 40; i = i + 8) {
+            if(i === 0){
+            document.querySelector(".city1").innerHTML = data5.city.name;
+            document.querySelector(".temp1").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
+            document.querySelector(".humidity1").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
+            document.querySelector(".wind1").innerHTML = Math.round(data5.list[i].wind.speed) + " km/h";
+          }else if(i === 8){
+            document.querySelector(".city2").innerHTML = data5.city.name;
+            document.querySelector(".temp2").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
+            document.querySelector(".humidity2").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
+            document.querySelector(".wind2").innerHTML = Math.round(data5.list[i].wind.speed) + " km/h";
+          }else if(i === 16){
+            document.querySelector(".city3").innerHTML = data5.city.name;
+            document.querySelector(".temp3").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
+            document.querySelector(".humidity3").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
+            document.querySelector(".wind3").innerHTML = Math.round(data5.list[i].wind.speed) + " km/h";
+          }else if(i === 24){
+            document.querySelector(".city4").innerHTML = data5.city.name;
+            document.querySelector(".temp4").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
+            document.querySelector(".humidity4").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
+            document.querySelector(".wind4").innerHTML = Math.round(data5.list[i].wind.speed) + " km/h";
+          }else if(i === 32){
+            document.querySelector(".city5").innerHTML = data5.city.name;
+            document.querySelector(".temp5").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
+            document.querySelector(".humidity5").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
+            document.querySelector(".wind5").innerHTML = Math.round(data5.list[i].wind.speed) + " km/h";
+          }
+          // console.log(data5.city.name)           //city name
+          // console.log(data5.list[i].dt_txt)      //date
+          // console.log(data5.list[i].main.temp)  //temp
+          // console.log(data5.list[i].main.humidity) //humidity
+          // console.log(data5.list[i].wind.speed) //wind-speed
 
           }
         })
