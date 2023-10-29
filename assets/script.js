ab8ab32d290c392da04ac7efb39fb1bc5e805cde
Tasneem <thalim.glam@gmail.com>
2023-10-28 22:50:07 -0700

    Updating upper container

--- assets/script.js
+++ assets/script.js
@@ -11,27 +11,26 @@ async function searchWeather(cityName) {
       return response.json()
     }).then(function (data) {
       console.log(data)
-    //  document.querySelector(".day").innerHTML = data.dt_txt.toLocaleString();
+      //  document.querySelector(".day").innerHTML = data.dt_txt.toLocaleString();
       document.querySelector(".city").innerHTML = data.name;
       document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
       document.querySelector(".humidity").innerHTML = Math.floor(data.main.humidity) + " %";
       document.querySelector(".wind").innerHTML = Math.ceil(data.wind.speed) + " km/h";
 
-      //--------------------------------- var currDate =  new Date(fiveDayArray[i].dt_txt).toLocaleString() ------------------------------------------------------------------------------     
+      //--------------------------------- var currDate =  new Date(fiveDayArray[i].dt_txt).toLocaleString().split(',')[0]; ------------------------------------------------------------------------------     
 
       const d = new Date();
       const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      function checkDay(day){
-        if(day + d.getDay() > 6){
-          return day + d.getDay()-7;
+      function checkDay(day) {
+        if (day + d.getDay() > 6) {
+          return day + d.getDay() - 7;
         }
-        else{
+        else {
           return day + d.getDay();
         }
       }
-      for(var i = 0; i<5; i++)
-      {
-        document.getElementById("day" + (i+1)).innerHTML = weekday[checkDay(i)]
+      for (var i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)]
       }
 
       var lat = data.coord.lat;
@@ -47,46 +46,45 @@ async function searchWeather(cityName) {
           for (i = 0; i < 40; i = i + 8) {
             if (i === 0) {
               //document.getElementById("img" +(i+1)).src=" https://openweathermap.org/img/wn/" + data5.list[i].weather[0].icon +".png"   
-
-              document.querySelector(".date1").innerHTML = data5.list[i].dt_txt;         
+              document.querySelector(".date1").innerHTML = (data5.list[i].dt_txt).split(" ")[0];
               document.querySelector(".city1").innerHTML = data5.city.name;
               document.querySelector(".temp1").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
               document.querySelector(".humidity1").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
               document.querySelector(".wind1").innerHTML = Math.round(data5.list[i].wind.speed) + " MPH";
             } else if (i === 8) {
-              document.querySelector(".date2").innerHTML = data5.list[i].dt_txt; 
+              document.querySelector(".date2").innerHTML = (data5.list[i].dt_txt).split(" ")[0];
               document.querySelector(".city2").innerHTML = data5.city.name;
               document.querySelector(".temp2").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
               document.querySelector(".humidity2").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
               document.querySelector(".wind2").innerHTML = Math.round(data5.list[i].wind.speed) + " MPH";
             } else if (i === 16) {
-              document.querySelector(".date3").innerHTML = data5.list[i].dt_txt; 
+              document.querySelector(".date3").innerHTML = (data5.list[i].dt_txt).split(" ")[0];
               document.querySelector(".city3").innerHTML = data5.city.name;
               document.querySelector(".temp3").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
               document.querySelector(".humidity3").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
               document.querySelector(".wind3").innerHTML = Math.round(data5.list[i].wind.speed) + " MPH";
             } else if (i === 24) {
-              document.querySelector(".date4").innerHTML = data5.list[i].dt_txt; 
+              document.querySelector(".date4").innerHTML = (data5.list[i].dt_txt).split(" ")[0];
               document.querySelector(".city4").innerHTML = data5.city.name;
               document.querySelector(".temp4").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
               document.querySelector(".humidity4").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
               document.querySelector(".wind4").innerHTML = Math.round(data5.list[i].wind.speed) + " MPH";
             } else if (i === 32) {
-              document.querySelector(".date5").innerHTML = data5.list[i].dt_txt; 
+              document.querySelector(".date5").innerHTML = (data5.list[i].dt_txt).split(" ")[0];
               document.querySelector(".city5").innerHTML = data5.city.name;
               document.querySelector(".temp5").innerHTML = Math.round(data5.list[i].main.temp) + "°F";
               document.querySelector(".humidity5").innerHTML = Math.floor(data5.list[i].main.humidity) + " %";
               document.querySelector(".wind5").innerHTML = Math.round(data5.list[i].wind.speed) + " MPH";
             }
             // console.log(data5.city.name)           //city name
-             //console.log((data5.list[i].dt_txt)      //date
+            //console.log((data5.list[i].dt_txt)      //date
             // console.log(data5.list[i].main.temp)  //temp
             // console.log(data5.list[i].main.humidity) //humidity
             // console.log(data5.list[i].wind.speed) //wind-speed
 
           }
         })
-     //-------- 5 days card ends here --------
+      //-------- 5 days card ends here --------
     })
     .catch(err)
   {
